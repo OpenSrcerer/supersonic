36427dc234d8fce00dc176249b462f5306a3f1a4
Weed-Pot <weedpot@fhf.edu.al>
2021-09-04 23:13:49 +0200

    [ci skip] Comment added

--- src/main/java/personal/opensrcerer/SupersonicApplication.java
+++ src/main/java/personal/opensrcerer/SupersonicApplication.java
@@ -2,6 +2,8 @@ package personal.opensrcerer;
 
 import personal.opensrcerer.launch.SupersonicLaunchWrapper;
 
+// A Discord Bot that allows playing music from Subsonic Servers.
+
 public class SupersonicApplication {
     public static void main(String[] args) throws Exception {
         SupersonicLaunchWrapper.run();
