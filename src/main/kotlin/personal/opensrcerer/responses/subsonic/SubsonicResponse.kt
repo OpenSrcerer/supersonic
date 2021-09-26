59672cda46883166d3b5999834a441a6337da107
OpenSrcerer <daniel.stefani@crispybacon.al>
2021-09-26 19:48:17 +0300

    Added request time variable to SubsonicResponse

--- src/main/kotlin/personal/opensrcerer/responses/subsonic/SubsonicResponse.kt
+++ src/main/kotlin/personal/opensrcerer/responses/subsonic/SubsonicResponse.kt
@@ -1,16 +1,25 @@
-package personal.opensrcerer.responses
+package personal.opensrcerer.responses.subsonic
 
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonProperty
 
-abstract class SubsonicResponse {
+open class SubsonicResponse {
     val status: String = ""
     val version: String = ""
     val xmlns: String = ""
     val error: SubsonicError? = null
+    private var requestTime: Long = 0
 
     class SubsonicError @JsonCreator constructor(
         @JsonProperty("code") val code: Int,
         @JsonProperty("message") val message: String
     )
+
+    fun setTime(t: Long) {
+        requestTime = t
+    }
+
+    fun getTime() : Long {
+        return requestTime
+    }
 }
