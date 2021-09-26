fb1a26aafdc7729a989c4f83853f676fa5308b11
OpenSrcerer <daniel.stefani@crispybacon.al>
2021-09-26 19:48:05 +0300

    Created async reactor logic for all Subsonic Requests

--- src/main/kotlin/personal/opensrcerer/client/SubsonicClient.kt
+++ src/main/kotlin/personal/opensrcerer/client/SubsonicClient.kt
@@ -1,83 +1,53 @@
 package personal.opensrcerer.client
 
-import com.fasterxml.jackson.dataformat.xml.XmlMapper
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import personal.opensrcerer.requests.RequestFormatter
-import personal.opensrcerer.requests.SubsonicRequest
-import personal.opensrcerer.requests.VoidRequest
-import personal.opensrcerer.requests.media.StreamRequest
-import personal.opensrcerer.responses.SubsonicResponse
+import personal.opensrcerer.requests.subsonic.SubsonicRequest
+import personal.opensrcerer.requests.subsonic.VoidRequest
+import personal.opensrcerer.responses.subsonic.SubsonicMonoCallback
+import personal.opensrcerer.responses.subsonic.SubsonicResponse
 import reactor.core.publisher.Mono
-import java.io.InputStream
 import java.util.concurrent.TimeUnit
+import java.util.logging.Level
 
 object SubsonicClient {
-
-    private val xmlMapper = XmlMapper()
     private val logger: Logger = LoggerFactory.getLogger(OkHttpClient::class.java)
     private val client: OkHttpClient
 
     init {
         logger.debug("Initializing OkHttp singleton for subsonic servers...")
         client = OkHttpClient().newBuilder()
-            .callTimeout(5000, TimeUnit.SECONDS)
+            .callTimeout(5000, TimeUnit.MILLISECONDS)
             .build()
     }
 
-    fun stream(req: StreamRequest, guildId: String) : Mono<InputStream?> {
-        val response = client.newCall(
-            Request.Builder()
-                .url(RequestFormatter.getUrl(req, guildId))
-                .build()
-        ).execute()
-
-        val type = response.header("content-type")
-        if (type.equals("text/xml")) {
-            return Mono.empty()
-        }
-
-        return Mono.justOrEmpty(response.body?.byteStream()).share()
+    fun request(req: VoidRequest, guildId: String): Mono<SubsonicResponse> {
+        return Mono.create<SubsonicResponse> { monoSink ->
+            client.newCall(
+                Request.Builder()
+                    .url(RequestFormatter.getUrl(req, guildId))
+                    .build()
+            ).enqueue(SubsonicMonoCallback(monoSink, SubsonicResponse::class.java)) }
+            .log("Subsonic Requests", Level.INFO)
     }
 
-    fun request(req: VoidRequest, guildId: String): SubsonicResponse {
-        val response = client.newCall(
-            Request.Builder()
-                .url(RequestFormatter.getUrl(req, guildId))
-                .build()
-        ).execute()
-
-        val body = response.body?.string()!!
-        logger.debug(body)
-
-        return this.parse(SubsonicResponse::class.java, body)
-    }
-
-    fun <T> request(req: SubsonicRequest<T>, guildId: String): T {
-        val response = client.newCall(
-            Request.Builder()
-                .url(RequestFormatter.getUrl(req, guildId))
-                .build()
-        ).execute()
-
-        val body = response.body?.string()!!
-        logger.debug(body)
-
-        return this.parse(req.getClazz(), body)
+    fun <T> request(req: SubsonicRequest<T>, guildId: String): Mono<T>
+    where T : SubsonicResponse {
+        return Mono.create<T> { monoSink ->
+            client.newCall(
+                Request.Builder()
+                    .url(RequestFormatter.getUrl(req, guildId))
+                    .build()
+            ).enqueue(SubsonicMonoCallback(monoSink, req.getClazz())) }
+            .log("Subsonic Requests", Level.INFO)
     }
 
     fun shutdown() {
         client.dispatcher.executorService.shutdown()
         client.connectionPool.evictAll()
         client.cache?.close()
-    }
-
-    private fun <T> parse(
-        clazz: Class<T>,
-        xml: String
-    ) : T {
-        return xmlMapper.readValue(xml, clazz)
     }
 }
