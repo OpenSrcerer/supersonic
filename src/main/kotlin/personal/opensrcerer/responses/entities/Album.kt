302fca06d79d40eecfc4ada2160cf15ab8fff20b
OpenSrcerer <daniel.stefani@crispybacon.al>
2021-09-26 16:52:24 +0300

    Combined Album, Artist and Song data objects under a common interface. Altered data model to fall back to null values from Subsonic

--- src/main/kotlin/personal/opensrcerer/responses/entities/Album.kt
+++ src/main/kotlin/personal/opensrcerer/responses/entities/Album.kt
@@ -1,12 +1,29 @@
 package personal.opensrcerer.responses.entities
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import personal.opensrcerer.responses.entities.enum.Unknown
 
-data class Album(
-    @JsonProperty("id") val id: Long,
-    @JsonProperty("parent") val parent: Long?,
-    @JsonProperty("title") val title: String?,
-    @JsonProperty("artist") val artist: String?,
-    @JsonProperty("isDir") val isDir: Boolean?,
-    @JsonProperty("coverArt") val coverArt: String?
-)
+class Album(
+    @JsonProperty("id")       id: String?,
+    @JsonProperty("parent")   parent: String?,
+    @JsonProperty("title")    title: String?,
+    @JsonProperty("artist")   artist: String?,
+    @JsonProperty("isDir")    isDir: String?,
+    @JsonProperty("coverArt") coverArt: String?
+) : EmbedMusicEntity {
+
+    val id = id ?: Unknown.ID.value
+    val parent = parent ?: Unknown.PARENT.value
+    val title = title ?: Unknown.TITLE.value
+    val artist = artist ?: Unknown.ARTIST.value
+    val isDir = isDir ?: Unknown.IS_DIRECTORY.value
+    val coverArt = coverArt ?: Unknown.COVER_ART.value
+
+    override fun embedName(): String {
+        return "[$id] / $title"
+    }
+
+    override fun embedValue(): String {
+        return artist
+    }
+}
