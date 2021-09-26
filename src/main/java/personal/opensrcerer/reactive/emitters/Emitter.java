0eceeb525bff90c19472c1ee6f04fa2b06d50cf5
OpenSrcerer <daniel.stefani@crispybacon.al>
2021-09-26 19:48:59 +0300

    Removed unused method calls

--- src/main/java/personal/opensrcerer/reactive/emitters/Emitter.java
+++ src/main/java/personal/opensrcerer/reactive/emitters/Emitter.java
@@ -3,17 +3,8 @@ package personal.opensrcerer.reactive.emitters;
 /**
  * Groups all Event Emitters together.
  */
+@FunctionalInterface
 public interface Emitter {
-    /**
-     * Makes the provided emitters subscribe to their respective sinks.
-     * @param emitters Emitters to call emit() for.
-     */
-    static void addListeners(Emitter... emitters) {
-        for (Emitter emitter : emitters) {
-            emitter.emit();
-        }
-    }
-
     /**
      * Subscribes to a sink and emits events when received.
      */
