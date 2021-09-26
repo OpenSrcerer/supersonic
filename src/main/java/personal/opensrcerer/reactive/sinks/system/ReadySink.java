3cecef966d7944f6ed8424602cebce1987c94abf
OpenSrcerer <daniel.stefani@crispybacon.al>
2021-09-26 19:49:37 +0300

    Removed initialization of emitters on startup, shifted to after readyevent

--- src/main/java/personal/opensrcerer/reactive/sinks/system/ReadySink.java
+++ src/main/java/personal/opensrcerer/reactive/sinks/system/ReadySink.java
@@ -1,18 +1,48 @@
 package personal.opensrcerer.reactive.sinks.system;
 
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.interactions.commands.Command;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.requests.RestAction;
 import personal.opensrcerer.config.SlashCommand;
 import personal.opensrcerer.launch.SupersonicConstants;
+import personal.opensrcerer.reactive.emitters.Emitter;
+import personal.opensrcerer.reactive.emitters.components.ButtonClickEmitter;
+import personal.opensrcerer.reactive.emitters.slash.misc.MumEmitter;
+import personal.opensrcerer.reactive.emitters.slash.player.*;
+import personal.opensrcerer.reactive.emitters.slash.system.PingEmitter;
+import personal.opensrcerer.reactive.emitters.system.ReadyEmitter;
 import personal.opensrcerer.reactive.sinks.Sink;
 
+import java.util.ArrayList;
 import java.util.EnumSet;
+import java.util.List;
+import java.util.Set;
 
 public class ReadySink implements Sink<ReadyEvent> {
     @Override
     public void onEvent(ReadyEvent readyEvent) {
+        initSlashCommands();
+        initEmitters();
+    }
+
+    private static void initEmitters() {
+        Set.of(
+                new ButtonClickEmitter(),
+                new MumEmitter(),
+                new JoinVoiceEmitter(),
+                new LeaveVoiceEmitter(),
+                new PauseEmitter(),
+                new PlayEmitter(),
+                new SearchEmitter(),
+                new SkipEmitter(),
+                new UnpauseEmitter(),
+                new PingEmitter()
+        ).forEach(Emitter::emit);
+    }
+
+    private static void initSlashCommands() {
         EnumSet<SlashCommand> commands = EnumSet.allOf(SlashCommand.class);
 
         if (commands.isEmpty()) {
@@ -41,6 +71,13 @@ public class ReadySink implements Sink<ReadyEvent> {
             command.getParameters().forEach((name, td) -> data.addOption(td.getLeft(), name, td.getRight(), true));
         }
 
+        String test = SupersonicConstants.getVariable("TEST_GUILD_ID");
+        if (test != null && !test.isBlank()) {
+            Guild guild = SupersonicConstants.getJDA().getGuildById(test);
+            if (guild != null) {
+                return guild.upsertCommand(data);
+            }
+        }
         return SupersonicConstants.getJDA().upsertCommand(data);
     }
 }
