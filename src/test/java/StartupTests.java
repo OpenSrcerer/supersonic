8b17a095192c25a43b239c72e8e855028d1d1d79
OpenSrcerer <daniel.stefani@crispybacon.al>
2021-12-26 20:56:25 +0100

    [FEAT] Started work on DB integration

--- src/test/java/StartupTests.java
+++ src/test/java/StartupTests.java
@@ -6,10 +6,14 @@ import personal.opensrcerer.launch.SupersonicConstants;
 import javax.security.auth.login.LoginException;
 
 public class StartupTests {
-
     @Test
     public void checkEnvironmentVariables() throws LoginException {
         SupersonicLaunchWrapper.run();
+
         Assertions.assertTrue(SupersonicConstants.isDefined("DISCORD_TOKEN"));
+        Assertions.assertTrue(SupersonicConstants.isDefined("POSTGRES_HOST"));
+        Assertions.assertTrue(SupersonicConstants.isDefined("POSTGRES_PASSWORD"));
+        Assertions.assertTrue(SupersonicConstants.isDefined("POSTGRES_USER"));
+        Assertions.assertTrue(SupersonicConstants.isDefined("POSTGRES_DB"));
     }
 }
