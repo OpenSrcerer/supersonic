74fab5df45909730dee017b9faa6cd9cebff0baf
CarbonMac <Austin76016@hotmail.com>
2022-03-04 13:24:48 -0600

    Imported constants, fixed variable case

--- src/main/kotlin/personal/opensrcerer/client/cache/SubsonicCache.kt
+++ src/main/kotlin/personal/opensrcerer/client/cache/SubsonicCache.kt
@@ -1,12 +1,13 @@
 package personal.opensrcerer.client.cache
+import personal.opensrcerer.launch.SupersonicConstants
 
 object SubsonicCache {
     private val cacheMap: MutableMap<String, SubsonicConfig>
-    private val url = System.getenv("sub_url") ?: "bonkersmusic.onthewifi.com"
-    private val port = System.getenv("sub_port").toInt() ?: 4040
-    private val user = System.getenv("sub_user") ?: "Bonkers"
-    private val pass = System.getenv("sub_pass") ?: "DNFWTF4201?!"
-    private val version = System.getenv("sub_version") ?: "1.15.0"
+    private val url = SupersonicConstants.getVariable("SUB_URL")!! ?: "bonkersmusic.onthewifi.com"
+    private val port = SupersonicConstants.getVariable("SUB_PORT")?.toInt()!! ?: 4040
+    private val user = SupersonicConstants.getVariable("SUB_USER")!! ?: "Bonkers"
+    private val pass = SupersonicConstants.getVariable("SUB_PASS")!! ?: "DNFWTF4201?!"
+    private val version =  SupersonicConstants.getVariable("SUB_VERSION")!! ?: "1.15.0"
 
     init {
         cacheMap = HashMap()
