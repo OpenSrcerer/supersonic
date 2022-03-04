3bfb36b5b7edc020b88c6d93b42f40622fdffc98
OpenSrcerer <daniel.stefani@crispybacon.al>
2022-03-04 21:35:39 +0200

    [ci skip] Inlined variables, removed force bang operator

--- src/main/kotlin/personal/opensrcerer/client/cache/SubsonicCache.kt
+++ src/main/kotlin/personal/opensrcerer/client/cache/SubsonicCache.kt
@@ -1,22 +1,18 @@
 package personal.opensrcerer.client.cache
+
 import personal.opensrcerer.launch.SupersonicConstants
 
 object SubsonicCache {
     private val cacheMap: MutableMap<String, SubsonicConfig>
-    private val url = SupersonicConstants.getVariable("SUB_URL")!! ?: "bonkersmusic.onthewifi.com"
-    private val port = SupersonicConstants.getVariable("SUB_PORT")?.toInt()!! ?: 4040
-    private val user = SupersonicConstants.getVariable("SUB_USER")!! ?: "Bonkers"
-    private val pass = SupersonicConstants.getVariable("SUB_PASS")!! ?: "DNFWTF4201?!"
-    private val version =  SupersonicConstants.getVariable("SUB_VERSION")!! ?: "1.15.0"
 
     init {
         cacheMap = HashMap()
         cacheMap["824772718800666645"] = SubsonicConfig(
-            url,
-            port,
-            user,
-            pass,
-            version
+            SupersonicConstants.getVariable("SUB_URL") ?: "bonkersmusic.onthewifi.com",
+            SupersonicConstants.getVariable("SUB_PORT")?.toInt() ?: 4040,
+            SupersonicConstants.getVariable("SUB_USER") ?: "Bonkers",
+            SupersonicConstants.getVariable("SUB_PASS") ?: "DNFWTF4201?!",
+            SupersonicConstants.getVariable("SUB_VERSION") ?: "1.15.0"
         )
     }
 
